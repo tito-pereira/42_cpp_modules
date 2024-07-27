9d1ea37d3ad543d6c9822a6b0d127f50358e1bc8
tito-pereira <tab.pereira00@gmail.com>
2024-07-27 22:09:57 +0200

    m

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -1,33 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/26 22:50:18 by marvin            #+#    #+#             */
-/*   Updated: 2024/07/26 22:50:18 by marvin           ###   ########.fr       */
+/*   Created: 2024/07/27 22:08:01 by marvin            #+#    #+#             */
+/*   Updated: 2024/07/27 22:08:01 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "megaphone.hpp"
-
-namespace   Megaphone {
-    ez;
-}
 
 int main(int argc, char **argv) {
     if (argc == 1) {
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return 0;
 	}
-	for (int i = 0; argv[i]; i++) {
+	for (int i = 1; argv[i]; i++) {
+		if (i != 1)
+			std::cout << ' ' << std::flush;
 		for (int j = 0; argv[i][j] != '\0'; j++) {
 			if (argv[i][j] >= 'a' && argv[i][j] <= 'z')
-				std::cout << argv[i][j] + 32 << std::endl;
+				std::cout << (char)(argv[i][j] - 32) << std::flush;
 			else
-				std::cout << argv[i][j] << std::endl;
+				std::cout << argv[i][j] << std::flush;
 		}
 	}
+	std::cout << std::endl;
 	return 1;
 }
