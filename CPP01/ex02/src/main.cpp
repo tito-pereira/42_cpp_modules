697636e1d561968256d1da786fc33ac2258240b6
tito-pereira <tab.pereira00@gmail.com>
2024-08-13 17:46:47 +0200

    final

--- CPP01/ex02/src/main.cpp
+++ CPP01/ex02/src/main.cpp
@@ -13,5 +13,13 @@
 #include "../include/this_is_brain.hpp"
 
 int main() {
-    ez;
+    std::string string = "HI THIS IS BRAIN";
+    std::string *stringPTR = &string;
+    std::string &stringREF = string;
+    std::cout << "og_string memory address: " << &string << std::endl;
+    std::cout << "stringPTR memory address: " << &stringPTR << std::endl;
+    std::cout << "stringREF memory address: " << &stringREF << std::endl;
+    std::cout << "og_string value: " << string << std::endl;
+    std::cout << "stringPTR value: " << *stringPTR << std::endl;
+    std::cout << "stringREF value: " << stringREF << std::endl;
 }
